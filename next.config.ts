4aee19c26aa60abfc1f7814a4158b50830abdc44
Vico Lee <vico.lee@coinbase.com>
2025-01-24 18:13:48 -0500

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
