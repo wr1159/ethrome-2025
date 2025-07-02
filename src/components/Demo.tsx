7e4a5be4af711942eeb3314e66cddfdbb40988e0
dylan-steck <dylan.steck@coinbase.com>
2025-07-02 11:41:07 -0400

    add manual-signin to test manually pasting a message and signature for siwf testing

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useFrameContext } from "~/components/providers/FrameProvider";
 import { useAccount } from "wagmi";
 import { SignInAction } from "~/components/actions/signin";
+import { ManualSignInAction } from "~/components/actions/manual-signin";
 import { QuickAuthAction } from "~/components/actions/quick-auth";
 import { OpenUrlAction } from "~/components/actions/openurl";
 import { FarcasterAction } from "~/components/actions/farcaster";
@@ -89,6 +90,7 @@ export default function Demo({ title = "CBW Mini App Demo" }: DemoProps) {
         {activeTab === "actions" && (
           <div>
             <SignInAction />
+            <ManualSignInAction />
             <QuickAuthAction />
             <OpenUrlAction />
             <FarcasterAction />
