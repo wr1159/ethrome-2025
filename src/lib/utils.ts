338be99b7d06393dc7dedab2eeae5817da5ef8c9
dylsteck <dylancsteck@gmail.com>
2025-07-24 15:50:00 -0400

    fix banner and icon images

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,8 @@ import { twMerge } from "tailwind-merge"
 export const METADATA = {
   name: "Base App Mini App Demo",
   description: "A demo mini app for testing capabilities in CBW",
-  bannerImageUrl: 'https://i.imgur.com/YcGZe2r.png',
-  iconImageUrl: 'https://i.imgur.com/jrz9fx0.png',
+  bannerImageUrl: 'https://i.imgur.com/2bsV8mV.png',
+  iconImageUrl: 'https://i.imgur.com/brcnijg.png',
   homeUrl: process.env.NEXT_PUBLIC_URL ?? "https://frames-v2-demo-lilac.vercel.app",
   splashBackgroundColor: "#000000"
 }
