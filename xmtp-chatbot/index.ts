e899cc72284ee75c5d4bd0213d5df6d7b47386a7
wr1159 <chuweiron18@gmail.com>
2025-10-19 08:35:38 +0200

    feat: show all members' pictures

--- xmtp-chatbot/index.ts
+++ xmtp-chatbot/index.ts
@@ -1,8 +1,14 @@
-// @ts-nocheck
-import { Agent } from "@xmtp/agent-sdk";
+import { Configuration, NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { Agent, IdentifierKind } from "@xmtp/agent-sdk";
 import { getTestUrl } from "@xmtp/agent-sdk/debug";
 
 process.loadEnvFile(".env");
+// Create resolver instance (uses web3.bio under the hood)
+const config = new Configuration({
+  apiKey: process.env.NEYNAR_API_KEY,
+});
+const client = new NeynarAPIClient(config);
+const FRONTEND_URL = "https://trick-or-treth.vercel.app";
 
 // 2. Spin up the agent
 const agent = await Agent.createFromEnv({
@@ -11,7 +17,33 @@ const agent = await Agent.createFromEnv({
 
 // 3. Respond to text messages
 agent.on("text", async (ctx) => {
-  await ctx.sendText("Hello from my XMTP Agent! 👋");
+  const message = ctx.message.content;
+  const members = await ctx.conversation.members();
+
+  // map all members to their address
+  const memberAddresses = members.map((member) => {
+    return member.accountIdentifiers.find(
+      (id) => id.identifierKind === IdentifierKind.Ethereum
+    )?.identifier;
+  });
+  const filteredMembersAddresses = memberAddresses.filter(
+    (member) => member !== "0x022bdbfe8e2da93d4153563e77b409805fa34a5f"
+  );
+  // [key: string]: Array<User>;
+  const nenynarUsers = await client.fetchBulkUsersByEthOrSolAddress({
+    addresses: filteredMembersAddresses,
+  });
+  const resolvedFids = Object.values(nenynarUsers)
+    .flat()
+    .map((user) => user.fid);
+  const filteredResolvedFids = resolvedFids.filter((fid) => fid !== 0);
+  console.log(filteredResolvedFids);
+
+  await ctx.sendText(`🚀 View in Mini App:`);
+  await ctx.sendText(`${FRONTEND_URL}`);
+  for (const fid of filteredResolvedFids) {
+    await ctx.sendText(`${FRONTEND_URL}/frame/${fid}`);
+  }
 });
 
 // 4. Log when we're ready
